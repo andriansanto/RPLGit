23d377090fe48a0d74ec7479d9ea10194aa7f4ac
ZefanyaWijaya <64416388+ZefanyaWijaya@users.noreply.github.com>
2020-04-27 21:37:03 +0700

    zefupdate

--- zef.php
+++ zef.php
@@ -1,3 +1,57 @@
 <!DOCTYPE html>
+<html>
+<head>
 <title>Zefanya Wijaya</title>
-<a href="home.php"></a>
+<meta charset="utf-8">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<style>
+.liar{
+   margin-left : 0px;
+   padding : 5px
+}
+.liar1{
+    margin-left : 0px;
+   padding : 5px
+}
+.aaa{
+    background: url('https://cdn.pixabay.com/photo/2017/12/28/15/06/background-3045402__340.png') no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+.card-2 {
+padding-top : 15px;
+box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+max-width: 950px;
+margin: auto;
+padding-bottom : 15px;
+text-align: center;
+font-family: arial;
+background-color: white;
+border-radius : 15px;
+margin-left : 427x;
+margin-top : 270px;
+}
+
+</style>
+</head>
+<body class = "aaa">
+    <nav class="navbar navbar-default">
+        <ul class="nav navbar-nav">
+            <li class = "liar" title ="Ga ada apa-apa"><a href="#">RPL</a></li>
+            <li class = "liar1" title ="Home"><a href="home.php">Home</a></li>
+            
+        </ul>
+    </nav>
+
+    <div class="card-2">
+        <h4>RPL_00000027182_ZefanyaWijaya_GitHub</h4>
+    </div>
+</body>
+
+</html>
