33373221b64d5a5513b1a654960a220c5ca3e2ed
DwynnTama <dwynn.tama@student.umn.ac.id>
2020-04-27 20:47:57 +0700

    'DwynnTama'

--- dwynn.php
+++ dwynn.php
@@ -1,5 +1,12 @@
 <!DOCTYPE html>
-
+<head>
+    <h1>Andrian Santo teman Zefanya Wijaya</h1> <br>
+    <h2 style="font-color:'blue'">NIM : 0000028024</h2> <br>
+    <h2 style="font-color:'blue'">Dwynn Tama</h2> <br>
+    <h2 style="font-color:'blue'">Kelompok Ask</h2> <br>
+</head>
 <a href ="home.php"></a>
+<p>Halo Saya Dwynn Tama!</p> <br>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, excepturi soluta, deleniti labore eos quisquam quaerat nam omnis facilis eligendi expedita nesciunt ratione. Saepe maxime reprehenderit quam, error omnis ducimus?</p>
 
 </html>
