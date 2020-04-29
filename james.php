9a6917d0d679c5c9397409ac905c7c47cfa2cfbe
jleopold88 <jleopold88@yahoo.com>
2020-04-29 11:29:54 +0700

    james

--- james.php
+++ james.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 
-<a href="home.php"></a>
-<p>asdadasdsa</p>
+<a href="home.php">Back to Home</a>
+<p>Ini James Leopold yang buat</p>
+<marquee behavior="" direction="">00000026766-RPL</marquee>
+
 
 </html>
